dbac7ff59e7cdb33fde5f8c087981cdfbc755ff9
hardeepsinhraj <hkraj0209@gmail.com>
2025-11-25 10:45:11 -0500

    updated lesson-12

--- lesson-12/js/charts.js
+++ lesson-12/js/charts.js
@@ -1,8 +1,7 @@
 // STEP 11: Get length of path for circle (circumference)
-let path = document.querySelector('svg #pieWedge');
+let path = document.querySelector("svg #pieWedge");
 console.log(path);
-let length = path.getTotalLength();
-
+let length = path.getTotalLength(); // Circumference!!!
 // STEP 12: Set the length of the space between dashes to the full circumference
 path.setAttribute("stroke-dasharray", "0 " + length);
 
@@ -11,30 +10,28 @@ path.setAttribute("stroke-dasharray", "0 " + length);
 let percent = document.querySelector("svg text").textContent;
 
 // STEP 13b: Remove the % sign from the text value
-percent = percent.replace("%", "");
-
+percent = percent.replaceAll("%", "");
 // STEP 13c: convert the string to an integer
-
 percent = parseInt(percent, 10);
-
+console.log(percent);
 // STEP 13d: Calculate width of pie segment as a percentage of the circumference of the circle - this will be used for the length of the dash part of the stroke
 percent = percent / 100;
-let pieLength = length * percent;
+let pieWidth = percent * length;
 
 // STEP 14: Grab the external CSS as one of the document object properties
-const cssRulesList = document.styleSheets[0];
-
-
+const cssRulesList = document.styleSheets[0].cssRules;
+console.log(cssRulesList);
 // STEP 15: Loop through the CSS rules to capture the one that controls the :active state for the circle
 let svgActiveRule;
-for (let i = 0; i < cssRulesList.length; i++) {
-    if(cssRulesList[i].SelectorText == "svg:active #pieWedge") {
+for (let i = 0; i < cssRulesList.length; i ++) {
+    if(cssRulesList[i].selectorText == "svg:active #pieWedge") {
+        // found the rule we want!!!
         svgActiveRule = cssRulesList[i];
-}}
+    }
+}
 
 // STEP 16: Build the string for the new CSS property value
 let propertyValue = pieWidth + " " + length;
 console.log(propertyValue);
-
 // STEP 17: Update the CSS declaration to reflect the new property value for stroke-dasharray
 svgActiveRule.style.setProperty("stroke-dasharray", propertyValue);
